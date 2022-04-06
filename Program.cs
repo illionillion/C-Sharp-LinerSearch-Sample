c4ebc0d0bebca9c7349c78733fee86df58ba981a
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-04-06 10:28:21 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int[] arr = new int[] {30,21,42,12,3,77,65,3};
+
+int find = 3;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (find==arr[i])
+    {
+        Console.WriteLine(i+"番目にあります");
+    }
+}
