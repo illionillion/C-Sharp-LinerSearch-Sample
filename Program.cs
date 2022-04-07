a301778a05b26620d50b19f64090abb1ce8efd7b
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-04-07 22:24:03 +0900

    標準入力はConsole.ReadLine()

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-int[] arr = new int[] {30,21,42,12,3,77,65,3};
+int[] arr = new int[] {30,21,42,12,3,77,3,10,65,3};
 
-int find = 3;
+Console.Write("探索する数値を入力>>>");
+// int find = 3;
+int find =int.Parse(Console.ReadLine());
 
+int count = 0;
 for (int i = 0; i < arr.Length; i++)
 {
     if (find==arr[i])
     {
         Console.WriteLine(i+"番目にあります");
+        count++;
     }
 }
+Console.WriteLine(count + "個見つかりました");
